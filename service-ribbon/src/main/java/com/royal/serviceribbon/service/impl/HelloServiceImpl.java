6751225a48edd702df5b2c5cf213f8332499b71f
huangrongya <huangrongya@foxmail.com>
2020-03-06 17:07:35 +0800

    ribbon 使用hystrix

--- service-ribbon/src/main/java/com/royal/serviceribbon/service/impl/HelloServiceImpl.java
+++ service-ribbon/src/main/java/com/royal/serviceribbon/service/impl/HelloServiceImpl.java
@@ -1,5 +1,6 @@
 package com.royal.serviceribbon.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.royal.serviceribbon.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,12 @@ public class HelloServiceImpl implements HelloService {
     RestTemplate restTemplate;
 
     @Override
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name){
         return restTemplate.getForObject("http://service-hi/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name){
+        return "hi "+name+", sorry, error";
     }
 }
